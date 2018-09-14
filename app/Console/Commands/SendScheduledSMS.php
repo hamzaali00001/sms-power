fd2f1b48760a4bfda993325f5977c03722a47153
Steve Gitau <stevegitau@gmail.com>
2018-09-14 11:20:45 +0300

    Fixes

--- app/Console/Commands/SendScheduledSMS.php
+++ app/Console/Commands/SendScheduledSMS.php
@@ -3,8 +3,10 @@
 namespace App\Console\Commands;
 
 use Carbon\Carbon;
+use App\Models\SentMessage;
 use App\Models\ScheduledMessage;
 use Illuminate\Console\Command;
+use AfricasTalking\SDK\AfricasTalking;
 
 class SendScheduledSMS extends Command
 {
@@ -39,6 +41,7 @@ class SendScheduledSMS extends Command
      */
     public function handle()
     {
+        /*
         ScheduledMessage::chunk(100, function ($messages) {
             foreach ($messages as $msg) {
                 if (Carbon::create(strtotime($msg->send_time))->between(Carbon::now()->subMinutes(5), Carbon::now())) {
@@ -46,5 +49,46 @@ class SendScheduledSMS extends Command
                 }
             }
         });
+        */
+
+        foreach (ScheduledMessage::all() as $msg) {
+            if ($msg->send_time && $msg->send_time->between(Carbon::now()->subMinutes(5),Carbon::now())) {
+                //dispatch(new \App\Jobs\SendScheduledSms($msg))->onQueue('send-scheduled-sms');
+                SentMessage::create([
+                    'user_id' => $msg->user_id,
+                    'from' => $msg->from,
+                    'to' => $msg->to,
+                    'message' => $msg->message,
+                    'msg_count' => $msg->msg_count,
+                    'characters' => $msg->msg_characters,
+                    'cost' => $msg->cost
+                ]);
+            }
+
+            // Send the message
+            $sms = $this->africastalking()->sms();
+
+            $sms->send([
+                'message' => $msg->message,
+                'to' => $msg->to,
+                'enqueue' => 'true'
+            ]);
+
+            // Delete the scheduled message;
+            $msg->delete();
+        }
+    }
+
+    /**
+     * Instantiate a new AfricasTalking instance.
+     *
+     * @return AfricasTalking\SDK\AfricasTalking;
+     */
+    protected function africastalking()
+    {
+        $username = env('AFRICASTALKING_USERNAME');
+        $apiKey = env('AFRICASTALKING_API_KEY');
+
+        return new AfricasTalking($username, $apiKey);
     }
 }

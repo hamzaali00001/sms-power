0530777f4d6c924478d7cfb2b01acff53678bfaf
Hamza Ali <hamza.ali00001@gmail.com>
2018-09-15 17:26:05 +0500

    Fixes

--- app/Jobs/UploadContacts.php
+++ app/Jobs/UploadContacts.php
@@ -23,6 +23,9 @@ class UploadContacts implements ShouldQueue
 
     protected $fileUpload;
     protected $group;
+    protected $invalidCount = [];
+    protected $validContactsCount;
+    protected $chunkSize = 100;
 
     /**
      * Create a new job instance.
@@ -35,6 +38,12 @@ class UploadContacts implements ShouldQueue
         $this->fileUpload = $fileUpload;
 
         $this->group = $group;
+
+        for ($i = 0; $i < 9; $i++) {
+            $this->invalidCount[$i] = 0;
+        }
+
+        $this->validContactsCount = 0;
     }
 
     /**
@@ -45,6 +54,8 @@ class UploadContacts implements ShouldQueue
     public function handle()
     {
         ini_set('memory_limit', '-1');
+        ini_set('max_execution_time', '500000');
+
         $storagePath = Storage::disk('local')->getDriver()->getAdapter()->getPathPrefix();
         $filePath = $storagePath . $this->fileUpload->location;
 
@@ -54,6 +65,7 @@ class UploadContacts implements ShouldQueue
             // Loading file as a spreadsheet
             $inputFileType = IOFactory::identify($filePath);
             $reader = IOFactory::createReader($inputFileType);
+            $reader->setReadDataOnly(true);
             $spreadsheet = $reader->load($filePath);
 
             $invalidData = collect();
@@ -67,6 +79,9 @@ class UploadContacts implements ShouldQueue
 
             // Message to send to the user
             $message = '';
+
+            // Invalid Data Count
+            $invalidDataCount = 0;
 
             foreach ($spreadsheet->getWorksheetIterator() as $index => $worksheet) {
                 $rowIterator = $worksheet->getRowIterator();
@@ -101,25 +116,46 @@ class UploadContacts implements ShouldQueue
                     // Validates data and populates valid and invalid arrays
                     $this->validateData($rowData, $validContacts, $invalidData);
                     $rowData = [];
+
+                    if ($validContacts->count() > $this->chunkSize) {
+                        $validContacts = $validContacts->unique('mobile');
+                        DB::table('contacts')->insert($validContacts->toArray());
+
+                        DB::commit();
+
+                        $this->validContactsCount += $validContacts->count();
+                        $validContacts = collect();
+                    }
+
+                    if ($invalidData->count() > $this->chunkSize) {
+                        $invalidDataCount += $invalidData->count();
+
+                        $this->addInvalidCount($invalidData);
+                        $invalidData = collect();
+                    }
                 }
 
                 // Removing Duplicates
                 $validContacts = $validContacts->unique('mobile');
 
                 // Database Seeding
-                foreach ($validContacts->chunk(1000) as $chunk) {
+                foreach ($validContacts->chunk($this->chunkSize) as $chunk) {
                     DB::table('contacts')->insert($chunk->toArray());
                 }
 
                 DB::commit();
             }
 
-            if ($invalidData->count() == 0 && (trim($message) == true)) { // Informing user about successful upload
+            $this->validContactsCount += $validContacts->count();
+
+            $this->addInvalidCount($invalidData);
+
+            if ($invalidDataCount == 0 && (trim($message) == true)) { // Informing user about successful upload
 //                $this->fileUpload->user->notify(new \App\Notifications\FileUploaded());
                 info('File uploaded successfully');
             } else {
                 //Creating Error message
-                $message = $this->createInvalidMessage($validContacts, $invalidData, $message);
+                $message = $this->createInvalidMessage($invalidData, $message);
 
                 info($message);
                 // Notifying user about all the files with failed uploads
@@ -206,10 +242,10 @@ class UploadContacts implements ShouldQueue
      * @param string $message
      * @return string
      */
-    private function createInvalidMessage($validContacts, $invalidData, $message)
+    private function createInvalidMessage($invalidData, $message)
     {
-        if ($validContacts->count() > 0) {
-            $message .= "Your file has uploaded <b>{$validContacts->count()} Contacts </b> with some errors!</br>";
+        if ($this->validContactsCount > 0) {
+            $message .= "Your file has uploaded <b>{$this->validContactsCount} Contacts </b> with some errors!</br>";
         } else {
             $message .= "Your file did not save any contact! You have some errors!";
         }
@@ -219,35 +255,77 @@ class UploadContacts implements ShouldQueue
         foreach ($invalidGroups as $type => $items) {
             switch ($type) {
                 case 'group_existing':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} existing contacts in group! </br>";
+                    if ($this->invalidCount[0] > 0) $message .= "Your file contains {$this->invalidCount[0]} existing contacts in group! </br>";
                     break;
                 case 'wrong_country':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} contacts that are not from Kenya! </br>";
+                    if ($this->invalidCount[1] > 0) $message .= "Your file contains {$this->invalidCount[1]} contacts that are not from Kenya! </br>";
                     break;
                 case 'not_mobile':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} that are not mobile! </br>";
+                    if ($this->invalidCount[2] > 0) $message .= "Your file contains {$this->invalidCount[2]} that are not mobile! </br>";
                     break;
                 case 'duplicate_file':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} duplicated contacts! </br>";
+                    if ($this->invalidCount[3] > 0) $message .= "Your file contains {$this->invalidCount[3]} duplicated contacts! </br>";
                     break;
                 case 'missing_country_code_error':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} contacts with missing country code! </br>";
+                    if ($this->invalidCount[4] > 0) $message .= "Your file contains {$this->invalidCount[4]} contacts with missing country code! </br>";
                     break;
                 case 'invalid_parameter_error':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} contacts with invalid parameterd! </br>";
+                    if ($this->invalidCount[5] > 0) $message .= "Your file contains {$this->invalidCount[5]} contacts with invalid parameterd! </br>";
                     break;
                 case 'number_format_error':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} contacts with wrong number format! </br>";
+                    if ($this->invalidCount[6] > 0) $message .= "Your file contains {$this->invalidCount[6]} contacts with wrong number format! </br>";
                     break;
                 case 'number_parser_error':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} contacts which number could not be parsed! </br>";
+                    if ($this->invalidCount[7] > 0) $message .= "Your file contains {$this->invalidCount[7]} contacts which number could not be parsed! </br>";
                     break;
                 case 'general':
-                    if ($items->count() > 0) $message .= "Your file contains {$items->count()} wrong contacts! </br>";
+                    if ($this->invalidCount[8] > 0) $message .= "Your file contains {$this->invalidCount[8]} wrong contacts! </br>";
                     break;
             }
         }
 
         return $message;
+    }
+
+    /**
+     * Adds Invalid data count
+     *
+     * @param Collection $invalidData
+     */
+    private function addInvalidCount($invalidData)
+    {
+        $invalidGroups = $invalidData->groupBy('type');
+
+        foreach ($invalidGroups as $type => $items) {
+            switch ($type) {
+                case 'group_existing':
+                    $this->invalidCount[0] += $items->count();
+                    break;
+                case 'wrong_country':
+                    $this->invalidCount[1] += $items->count();
+                    break;
+                case 'not_mobile':
+                    $this->invalidCount[2] += $items->count();
+                    break;
+                case 'duplicate_file':
+                    $this->invalidCount[3] += $items->count();
+                    break;
+                case 'missing_country_code_error':
+                    $this->invalidCount[4] += $items->count();
+                    break;
+                case 'invalid_parameter_error':
+                    $this->invalidCount[5] += $items->count();
+                    break;
+                case 'number_format_error':
+                    $this->invalidCount[6] += $items->count();
+                    break;
+                case 'number_parser_error':
+                    $this->invalidCount[7] += $items->count();
+                    break;
+                case 'general':
+                    $this->invalidCount[8] += $items->count();
+                    break;
+            }
+        }
     }
 }
